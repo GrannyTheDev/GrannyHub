011609605a1680e0ac62caf46753480bb2b21c9e
GrannyTheDev <milansnel2001@gmail.com>
2022-10-17 10:41:13 +0200

    master

--- Modules/Obfuscater.lua
+++ Modules/Obfuscater.lua
@@ -1,7 +1,7 @@
 local Obfuscater = {}
 
 function Obfuscation(code)
-local encode = code:gsub(".", function(bb) return "\\" .. bb:byte() end) or code .. "\""
+local encode = tostring(code:gsub(".", function(bb) return "\\" .. bb:byte() end) or code .. "\"")
 setclipboard(code)
 end
 
