8e14ea7b9870bf1261782db9f3b8539f30a54734
GrannyTheDev <100591809+GrannyTheDev@users.noreply.github.com>
2022-10-23 12:29:21 +0200

    master

--- Shit.lua
+++ Shit.lua
@@ -1,24 +1,24 @@
 if gethui then
 	for i,v in pairs(gethui():GetChildren()) do
-		if v:IsA("ScreenGui") and v.Name == "DarkLib" or v.Name == "Gui" or v.Name == "ScreenGui" then
-			v:Destroy()
-		end
+	     if v:IsA("ScreenGui") and v.Name == "DarkLib" then
+		 v:Destroy()
+	     end
 	end
 elseif syn and syn.unprotect_gui then
 	for i,v in pairs(game.CoreGui:GetChildren()) do
-		if v:IsA("ScreenGui") and v.Name == "DarkLib" then
-                        syn.unprotect_gui(v)
-                end
-                if v:IsA("ScreenGui") and v.Name == "DarkLib" or v.Name == "Gui" or v.Name == "ScreenGui" then
-                        v:Destroy()
-		end
+	     if v:IsA("ScreenGui") and v.Name == "DarkLib" then
+                 syn.unprotect_gui(v)
+             end
+             if v:IsA("ScreenGui") and v.Name == "DarkLib" then
+                 v:Destroy()
+	     end
 	end
 else
 	for i,v in pairs(game.CoreGui:GetChildren()) do
-		if v:IsA("ScreenGui") and v.Name == "DarkLib" or v.Name == "Gui" or v.Name == "ScreenGui" then
-			v:Destroy()
-		end
-	end
+	     if v:IsA("ScreenGui") and v.Name == "DarkLib" then
+		 v:Destroy()
+	     end
+        end
 end
 
 repeat wait() until game and game:IsLoaded()
