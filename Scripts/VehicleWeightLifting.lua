04b195f3870d557cfafba5c8194ff4b92f31f141
GrannyTheDev <100591809+GrannyTheDev@users.noreply.github.com>
2022-10-24 12:00:51 +0200

    Update VehicleWeightLifting.lua

--- Scripts/VehicleWeightLifting.lua
+++ Scripts/VehicleWeightLifting.lua
@@ -8,8 +8,6 @@ elseif syn and syn.unprotect_gui then
 	for i,v in pairs(game.CoreGui:GetChildren()) do
 	     if v:IsA("ScreenGui") and v.Name == "Gui" then
                  syn.unprotect_gui(v)
-             end
-             if v:IsA("ScreenGui") and v.Name == "Gui" then
                  v:Destroy()
 	     end
 	end
