d8d31c9ca9a421681234ce6e3d2d4723f85edba9
GrannyTheDev <milansnel2001@gmail.com>
2022-11-21 18:55:49 +0100

    master

--- GrannyHub.lua
+++ GrannyHub.lua
@@ -38,8 +38,9 @@ local games = {
 	["BloxFruit.lua"] = 2753915549,
 	["PrisonLife.lua"] = 155615604,
 	["UltimateLiftingSimulator.lua"] = 2472820296,
-        ["ShadovisRpg.lua"] = 9585537847,
-        ["TextingSimulator.lua"] = 2580982329
+    ["ShadovisRpg.lua"] = 9585537847,
+    ["TextingSimulator.lua"] = 2580982329,
+	["GorillaSimulator.lua"] = 1378712700
 }
 
 local obbygames = {
