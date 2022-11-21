fc05bbbef0b2dc881cf06c9125f82675a332feaf
GrannyTheDev <milansnel2001@gmail.com>
2022-11-21 17:43:22 +0100

    master

--- Scripts/DrinkHaxx.lua
+++ Scripts/DrinkHaxx.lua
@@ -73,7 +73,6 @@ end
 function doAntiKick()
 spawn(function()
 if getgenv().Settings.antikick == true then
-        if hydrogen then
 	local mt = getrawmetatable(game)
 	local old = mt.__namecall
 	local protect = newcclosure or protect_function
@@ -88,14 +87,6 @@ if getgenv().Settings.antikick == true then
 		return old(self, ...)
 	end)
 	hookfunction(game.Players.LocalPlayer.Kick,protect(function() wait(9e9) end))
-        else
-        for i,v in pairs(getconnections(game.Players.LocalPlayer.PlayerGui.AntiAutoclick.TextButton.MouseButton1Click)) do
-          v.Function()
-        end
-       end
-while wait() do
-game.Players.LocalPlayer.PlayerGui:WaitForChild("AntiAutoclick").Enabled = false
-end
 end
 end)
 end
