421454a1a9a8b53f24f7f166675ea7592ef36778
GrannyTheDev <milansnel2001@gmail.com>
2022-11-26 08:25:55 +0100

    master

--- GrannyHub.lua
+++ GrannyHub.lua
@@ -43,7 +43,8 @@ local games = {
 	["GorillaSimulator.lua"] = 1378712700,
 	["ZombieAttack.lua"] = 1240123653,
 	["DestructionSimulator.lua"] = 2248408710,
-	["HideandSeekExtreme.lua"] = 205224386
+	["HideandSeekExtreme.lua"] = 205224386,
+	["MuscleLegends.lua"] = 3623096087
 }
 
 local obbygames = {
