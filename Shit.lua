cfb98e84324f24d2f249f821d7001bd418fa14ab
GrannyTheDev <100591809+GrannyTheDev@users.noreply.github.com>
2022-12-26 19:23:50 +0100

    Update Shit.lua

--- Shit.lua
+++ Shit.lua
@@ -69,7 +69,14 @@ function Library:CreateWindow(txt)
 				end
 			end
 		end
-	end)
+	en
+
+        getgenv().color = {
+            header = Color3.fromRGB(25, 25, 25);
+            MainFrame = Color3.fromRGB(35, 35, 35);
+            dragui = Color3.fromRGB(35, 35, 35);
+            TitleColor = Color3.fromRGB(255, 255, 255);
+        }
 
 	Header.Name = "Header"
 	Header.Parent = GrannyHub
